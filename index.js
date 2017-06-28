275469c17f7e2fb04177ac763c4ab2b05d83eabe
arve0 <arve.seljebu@gmail.com>
2017-06-28 22:32:08 +0200

    use object-path for paths

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const escape = require('escape-string-regexp')
+const objectPath = require('object-path')
 const deepReduce = require('deep-reduce')
 
 /**
@@ -29,7 +30,7 @@ const deepReduce = require('deep-reduce')
 module.exports = function (options = {}) {
   return function (hook) {
     if (hook.method === 'find' && hook.params.query && hook.params.query.$search) {
-      hook.params.query.$where = fuzzySearch(hook.params.query.$search, options.fields)
+      hook.params.query.$where = fuzzySearch(hook.params.query.$search, options)
       delete hook.params.query.$search
     }
     return hook
@@ -44,16 +45,42 @@ module.exports = function (options = {}) {
  * @param {string} str search for this string
  * @return {function}
  */
-function fuzzySearch (str, fields) {
+function fuzzySearch(str, { fields, deep }) {
   let r = new RegExp(escape(str), 'i')
 
-  return function () {
-    let result = deepReduce(this, (hit, value, path) =>
-      hit || (
-        (fields === undefined || fields.include(path)) &&
-        typeof value === 'string' &&
-        value.match(r) !== null
-      ), false)
-    return result
+  if (Array.isArray(fields)) {
+    return function () {
+      return fields.reduce((match, field) => {
+        if (match === true) {
+          return true
+        }
+        let value = objectPath.get(this, field)
+        return typeof value === 'string' && value.match(r) !== null
+      }, false)
+    }
   }
+
+  if (deep) {
+    return function () {
+      let result = deepReduce(this, (match, value) =>
+        match || (
+          typeof value === 'string' && value.match(r) !== null
+        ), false)
+      return result
+    }
+  }
+
+  return function () {
+    for (let key in this) {
+      // do not search _id and similar fields
+      if (key[0] === '_' || !this.hasOwnProperty(key)) {
+        continue
+      }
+      if (typeof this[key] === 'string' && this[key].match(r)) {
+        return true
+      }
+    }
+    return false
+  }
+
 }
--- test.js
+++ test.js
@@ -5,7 +5,12 @@ const NeDBservice = require('feathers-nedb')
 const search = require('./')
 const assert = require('assert')
 
-let service
+let app, service, res
+
+let _currentHook = search()
+function currentHook (hook) {
+  return _currentHook(hook)
+}
 
 before(async function () {
   const Model = new NeDB({
@@ -18,41 +23,70 @@ before(async function () {
   app.use('/test', NeDBservice({ Model, id: 'kode' }))
   app.hooks({
     before: {
-      find: search()
+      find: currentHook
     }
   })
 
   service = app.service('test')
 
-  await service.create({
-    kode: 'test',
-    some: { nested: { path: 'with string to search for' } }
-  })
+  await service.create([
+    {
+      kode: 'test',
+      some: { nested: { path: 'with string to search for' } }
+    }, {
+      kode: 'test2',
+      another: { nested: { path: 'with string to search for' } }
+    }
+  ])
 })
 
 after(function remove (done) {
-  service.remove('test').then(() => {
+  Promise.all([
+    service.remove('test'),
+    service.remove('test2')
+  ])
+  .then(() => {
     service.Model.persistence.persistCachedDatabase(done)
   })
 })
 
 it ('should find three documents with the text `pytagoras` in them.', async function () {
   this.timeout(100)
-  let res = await service.find({ query: { $search: 'pytagoras' } })
+  res = await service.find({ query: { $search: 'pytagoras' } })
   assert.equal(res.length, 3)
 })
 
 it('should search documents deeply', async function () {
-  let res = await service.find({ query: { $search: 'with string to search for' } })
+  _currentHook = search({
+    deep: true
+  })
+  res = await service.find({ query: { $search: 'with string to search for' } })
+  assert.equal(res.length, 2)
+})
+
+it('should allow specifying field names to search', async function () {
+  _currentHook = search({
+    fields: ['some.nested.path']
+  })
+  res = await service.find({ query: { $search: 'with string to search for' } })
   assert.equal(res.length, 1)
 })
 
+it('should not find anything when specifying non existent path', async function () {
+  _currentHook = search({
+    fields: ['non.existent.path']
+  })
+  res = await service.find({ query: { $search: 'with string to search for' } })
+  assert.equal(res.length, 0)
+})
+
 it('should perform well', async function () {
+  _currentHook = search()
   let times = 50
-  let maxTime = 50
+  let maxTime = 100
   this.timeout(maxTime * (times + 1))
   let totalTime = 0
-  let res, start
+  let start
   for (let i = 0; i < times; i++) {
     start = Date.now()
     res = await service.find({ query: { $search: 'pytagoras' } })
